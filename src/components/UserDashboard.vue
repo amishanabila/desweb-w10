5d5d8d4c52056f0608fe2035026878f345cb4729
amishanabila <168618496+amishanabila@users.noreply.github.com>
2025-10-30 12:11:53 +0800

    Add files via upload

---
+++ src/components/UserDashboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard">
+    <h1>Welcome to the Dashboard</h1>
+    <p>You are logged in.</p>
+    <button @click="logout">Logout</button> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserDashboard',
+  methods: {
+    logout() {
+      this.$emit('logout');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.dashboard {
+  text-align: center;
+}
+
+p {
+  font-size: 22px;
+}
+
+button {
+  font-size: 18px; 
+  padding: 3px 9px; 
+  border-radius: 3px;
+}
+</style>
---
+++ src/components/UserLogin.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="login">
+    <h1>Login</h1> 
+    <form @submit.prevent="handleLogin">
+      <div>
+        <label for="username" class="label">Username:</label>
+        <input type="text" v-model="username" required class="input-field" />
+      </div>
+      <div>
+        <label for="password" class="label">Password:</label>
+        <input type="password" v-model="password" required class="input-field" />
+      </div>
+      <button type="submit" class="login-button">Login</button>
+      <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      errorMessage: '' 
+    };
+  },
+  methods: {
+    handleLogin() {
+      if (this.username === 'admin' && this.password === 'admin') {
+        this.$emit('loginSuccess');
+        this.errorMessage = '';
+      } else {
+        this.errorMessage = 'Invalid username or password';
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.error {
+  color: red;
+}
+
+.label {
+  font-size: 20px; 
+}
+
+.input-field {
+  height: 25px; 
+  width: 250px; 
+  font-size: 16px; 
+  border-radius: 3px;
+}
+
+.login-button {
+  font-size: 18px; 
+  padding: 5px 8px; 
+  border-radius: 3px;
+}
+</style>
